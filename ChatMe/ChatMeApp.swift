b674816ec800e0ca8ae004e0c17535e86699f78b
Aleksandar Milidrag <salkaorama@gmail.com>
2025-03-28 18:23:30 +0100

    Initial Commit

---
+++ ChatMe/ChatMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatMeApp.swift
+//  ChatMe
+//
+//  Created by Aleksandar Milidrag on 28. 3. 2025..
+//
+
+import SwiftUI
+
+@main
+struct ChatMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatMeTests/ChatMeTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChatMeTests.swift
+//  ChatMeTests
+//
+//  Created by Aleksandar Milidrag on 28. 3. 2025..
+//
+
+import Testing
+@testable import ChatMe
+
+struct ChatMeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
