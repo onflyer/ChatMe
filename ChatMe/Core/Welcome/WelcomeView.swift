d0cc170c6fa99c7cfd3ba5e80051300c687ae21b
Aleksandar Milidrag <salkaorama@gmail.com>
2025-03-31 18:09:49 +0200

    Update WelcomeView.swift

--- ChatMe/Core/Welcome/WelcomeView.swift
+++ ChatMe/Core/Welcome/WelcomeView.swift
@@ -13,62 +13,35 @@ struct WelcomeView: View {
     
     var body: some View {
         VStack {
-                       Text("Welcome to ChatME")
-                           .font(.largeTitle)
-                           .fontWeight(.bold)
-                           .padding(.vertical, 50)
-                           .multilineTextAlignment(.center)
-                       Spacer()
-                       VStack {
-                               VStack(alignment: .leading) {
-                                   HStack {
-                                           Image(systemName: "bubble.left.and.bubble.right.fill")
-                                               .resizable()
-                                               .aspectRatio(contentMode: .fit)
-                                               .frame(width: 45, alignment: .center)
-                                               .clipped()
-                                               .foregroundColor(.primary)
-                                               .padding(.trailing, 15)
-                                               .padding(.vertical, 10)
-                                       
-                                       VStack(alignment: .leading) {
-                                           Text("Title")
-                                               .fontWeight(.bold)
-                                               .font(.system(size: 16))
-                                           Text("Description")
-                                               .font(.system(size: 15))
-                                       }
-                                       Spacer()
-                                   }
-                               }
-                               .padding(.horizontal,20)
-                               .padding(.bottom, 20)
-                           
-                       }
-                       .padding(.bottom, 30)
-                       Spacer()
-                       VStack {
-                           ZStack {
-                               Rectangle()
-                                   .foregroundColor(.primary)
-                                   .cornerRadius(12)
-                                   .frame(height: 54)
-                               Text("Continue")
-                                   .fontWeight(.bold)
-                                   .foregroundColor(.white)
-                           }
-                       }
-                       .padding(.top, 15)
-                       .padding(.bottom, 50)
-                       .padding(.horizontal,15)
-                   }
-                   .padding()
-            .onAppear {
-                presenter.onViewAppear(delegate: delegate)
+            headerText
+            
+            Spacer()
+            
+            content
+            
+            Spacer()
+            VStack {
+                ZStack {
+                    Rectangle()
+                        .foregroundColor(.primary)
+                        .cornerRadius(12)
+                        .frame(height: 54)
+                    Text("Continue")
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                }
             }
-            .onDisappear {
-                presenter.onViewDisappear(delegate: delegate)
-            }
+            .padding(.top, 15)
+            .padding(.bottom, 50)
+            .padding(.horizontal,15)
+        }
+        .padding()
+        .onAppear {
+            presenter.onViewAppear(delegate: delegate)
+        }
+        .onDisappear {
+            presenter.onViewDisappear(delegate: delegate)
+        }
     }
 }
 
@@ -83,3 +56,96 @@ struct WelcomeView: View {
     }
 }
 
+
+extension WelcomeView {
+    var headerText: some View {
+        Text("Welcome to ChatME")
+            .font(.largeTitle)
+            .fontWeight(.bold)
+            .padding(.vertical, 50)
+            .multilineTextAlignment(.center)
+    }
+    
+    var content: some View {
+        VStack {
+            VStack(alignment: .leading) {
+                HStack {
+                    Image(systemName: "bubble.left.and.bubble.right.fill")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 45, alignment: .center)
+                        .clipped()
+                        .foregroundColor(.primary)
+                        .padding(.trailing, 15)
+                        .padding(.vertical, 10)
+                    
+                    VStack(alignment: .leading) {
+                        Text("Title")
+                            .fontWeight(.bold)
+                            .font(.system(size: 16))
+                        Text("Description")
+                            .font(.system(size: 15))
+                    }
+                    Spacer()
+                }
+            }
+            .padding(.horizontal,20)
+            .padding(.bottom, 20)
+            
+            VStack(alignment: .leading) {
+                HStack {
+                    Image(systemName: "bubble.left.and.bubble.right.fill")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 45, alignment: .center)
+                        .clipped()
+                        .foregroundColor(.primary)
+                        .padding(.trailing, 15)
+                        .padding(.vertical, 10)
+                    
+                    VStack(alignment: .leading) {
+                        Text("Title")
+                            .fontWeight(.bold)
+                            .font(.system(size: 16))
+                        Text("Description")
+                            .font(.system(size: 15))
+                    }
+                    Spacer()
+                }
+            }
+            .padding(.horizontal,20)
+            .padding(.bottom, 20)
+            
+            VStack(alignment: .leading) {
+                HStack {
+                    Image(systemName: "bubble.left.and.bubble.right.fill")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 45, alignment: .center)
+                        .clipped()
+                        .foregroundColor(.primary)
+                        .padding(.trailing, 15)
+                        .padding(.vertical, 10)
+                    
+                    VStack(alignment: .leading) {
+                        Text("Title")
+                            .fontWeight(.bold)
+                            .font(.system(size: 16))
+                        Text("Description")
+                            .font(.system(size: 15))
+                    }
+                    Spacer()
+                }
+            }
+            .padding(.horizontal,20)
+            .padding(.bottom, 20)
+            
+            
+            
+        }
+        .padding(.bottom, 30)
+
+    }
+
+    
+}
